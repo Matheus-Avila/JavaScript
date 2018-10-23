0234773794d0aa3b3eaf9fbeb5c2a17a23f1ef94
Matheus Avila <matheusavila1997@gmail.com>
2018-10-23 12:56:30 +0000

    Curso completo

--- J2_StructuringSafeCode/Starter/Greetr.js
+++ J2_StructuringSafeCode/Starter/Greetr.js
@@ -4,6 +4,23 @@
         return new Greetr.init(nome, sobrenome, lingua);
     }
 
+    var supportedLangs = ['en', 'br'];
+
+    var greetings = {
+        en: 'Hi',
+        br: 'oi'
+    } 
+
+    var formalGreetings = {
+        en: 'Hello',
+        br: 'Ola'
+    }
+
+    var logMessage = {
+        en: 'Logged in',
+        br: 'Logado'
+    }
+
     Greetr.init = function(nome, sobrenome, lingua){
 
         this.nome = nome || "Padrao" ;
@@ -14,7 +31,92 @@
     }
     //A function constructor define o prototype como sendo um objeto Greetr.init.
     //As linhas 17 e 19 definem o prototype como sendo um objeto vazio e sem 'classe'.
-    Greetr.prototype = {};
+    Greetr.prototype = {
+
+        fullName: function(){
+            return this.nome + ' ' + this.sobrenome;
+        },
+
+        valida: function(){
+            return (supportedLangs.indexOf(this.lingua) !== -1)
+        },
+        
+        greeting: function(){
+            if(this.valida()){
+                return greetings[this.lingua] + ' ' + this.nome;  
+            }
+            
+        },
+
+        greetFormal: function(){
+            return formalGreetings[this.lingua] + ' ' + this.fullName();
+        },
+        
+        greet: function(formal){
+            var msg;
+
+            if(formal){
+                msg = this.greetFormal();
+            }
+            else {
+                msg = this.greeting();
+            }
+
+            console.log(msg);
+
+            //Função chainable
+            return this;
+        },   
+
+        log: function(){
+
+            console.log(logMessage[this.lingua] + ': ' + this.fullName());
+            
+            return this;
+        },
+
+        setLang: function(lang){
+            var lingua = this.lingua;
+            this.lingua = lang;
+            
+            if(this.valida()){
+                //Não torna a função chainable:
+                //return this;
+            }
+            else{
+                console.log('Operacao nao concluida!\nLingua nao suportada pelo framework!!');
+                this.lingua = lingua;
+            }
+            //Para ser chainable, o return deve ser colocado na última linha, fora de qualquer condicional
+            return this;
+
+        },
+
+        HTMLGreeting: function(selector, formal){
+            var msg;
+
+            if(formal){
+                msg = this.greetFormal();
+            }
+            else {
+                msg = this.greeting();
+            }
+
+            $(selector).html(msg);
+
+            return this;
+        }
+
+
+
+
+
+
+
+
+
+
+    };
 
     Greetr.init.prototype = Greetr.prototype;
 
--- J2_StructuringSafeCode/Starter/app.js
+++ J2_StructuringSafeCode/Starter/app.js
@@ -1,3 +1,21 @@
-var g = $G('Mat','Luc','Thi');
+/*var g = $G('Matheus','avila');
 
-console.log(g)
+console.log(g);
+
+g.greet(true).setLang('en');
+
+g.greet(true);
+
+g.setLang('br').greet(false);*/
+
+$('#login').click(function(){
+
+    var loginObj = $G('Matheus', 'Avelar');
+
+    $('#logindiv').hide();
+    //loginObj.setLang($('#lang').val()): define a lingua como o val do item que possui o id lang
+    //Retorna loginObj 
+    //loginObj.HTMLGreeting('#greeting', true) escreve em html no elemento com id greeting
+    loginObj.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+
+});
